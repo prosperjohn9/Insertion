72fc99a05ad2559608a638babe3187a47e42818a
prosperjohn9 <prosperjohn9@gmail.com>
2024-01-05 08:41:28 +0000

    updated

--- .ccls-cache/@home@runner@Insertion/main.c
+++ .ccls-cache/@home@runner@Insertion/main.c
@@ -2,24 +2,25 @@
 #include <string.h>
 
 int main() {
-  int arr[] = {1, 3, 5, 7, 9};
+  int arr[6] = {1, 3, 5, 7, 9};
   int k = 4, item = 8;
+  int size = sizeof(arr) / sizeof(arr[0]);
 
   printf("-------- Array elements before insertion --------\n");
-  for (int i = 0; i < sizeof(arr)/sizeof(arr[0]); i++){
-    printf(" Element in arr [%d] is: %d\n", i, arr[i]);
+  for (int i = 0; i < size - 1; i++) {
+    printf(" Element in array [%d] is: %d\n", i, arr[i]);
   }
-  int j = sizeof(arr)/sizeof(arr[0]);
-  
+  int j = sizeof(arr) / sizeof(arr[0]);
+
   while (j >= k) {
-    arr[j+1] = arr[j];
+    arr[j + 1] = arr[j];
     j--;
   }
   arr[k] = item;
 
   printf("-------- Array elements after insertion --------\n");
-  for (int i = 0; i < sizeof(arr)/sizeof(arr[0]) + 1; i++){
-    printf(" Element in arr [%d] is: %d\n", i, arr[i]);
+  for (int i = 0; i < size; i++) {
+    printf(" Element in array [%d] is: %d\n", i, arr[i]);
   }
   return 0;
 }
